8ff38ab0c6696b53813e9eeb03d5bbd09ae69742
SohamVermaDev <shinehtet19aung+@gmail.com>
2026-06-16 18:15:57 +0630

    chore(config): configure FontAwesome for admin dashboard
    Install vue-fontawesome packages and register 7 icons globally in main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 import './styles/global.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faHome, faUsers, faCubesStacked, faCartShopping, faChartPie, faGear } from '@fortawesome/free-solid-svg-icons' // insert only the needed icons
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' 
 
-createApp(App).mount('#app');
+library.add(faBars, faHome, faUsers, faCubesStacked, faCartShopping, faChartPie, faGear)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
