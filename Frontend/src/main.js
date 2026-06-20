18779e3faf5668d162141714ff88a00d91c4585d
SohamVermaDev <shinehtet19aung+@gmail.com>
2026-06-20 09:32:20 +0630

    chore(config): remove FontAwesome Vue package in favor of CDN
    Uninstall vue-fontawesome packages, clean up main.js, and add CDN link for simpler icon usage

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,12 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/global.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faHome, faUsers, faCubesStacked, faCartShopping, faChartPie, faGear } from '@fortawesome/free-solid-svg-icons' // insert only the needed icons
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' 
-
-library.add(faBars, faHome, faUsers, faCubesStacked, faCartShopping, faChartPie, faGear)
+import { createApp } from 'vue';
+import App from './App.vue';
+import './styles/global.scss';
 
 const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
