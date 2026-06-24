7e8953817899e3f4e4df0f5d44a1b4a2515bfbab
SohamVermaDev <shinehtet19aung+@gmail.com>
2026-06-24 17:07:12 +0630

    chore(deps): add Orbitron font via Fontsource
    Install @fontsource/orbitron for NeoForge logo

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './styles/global.scss';
 import router from './router/index.js';
+import './styles/global.scss';
+import '@fontsource/orbitron/700.css';
 
 const app = createApp(App);
 app.use(router);
