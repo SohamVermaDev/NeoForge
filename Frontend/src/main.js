904b09a507e1ae56613258083508be01ac3c0700
SohamVermaDev <shinehtet19aung+@gmail.com>
2026-08-20 20:50:57 +0630

    fix(config): add Pinia to Vue app
    Add createPinia and app.use(pinia) in main.js to enable Pinia store functionality

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router/index.js";
 import "./styles/global.scss";
 import "@fontsource/orbitron/700.css";
 
 const app = createApp(App);
+app.use(createPinia());
 app.use(router);
 app.mount("#app");
